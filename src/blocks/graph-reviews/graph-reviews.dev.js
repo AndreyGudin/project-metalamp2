aed4740f0a73be3b6a06d8f81c690ff72bcd0d24
Andrey_Gudin <aeon150791@gmail.com>
2021-11-26 11:55:06 +0400

    Finished graphic-reviews. Added custom gradient for legends, added gradient for arcs

--- src/blocks/graph-reviews/graph-reviews.dev.js
+++ src/blocks/graph-reviews/graph-reviews.dev.js
@@ -2,14 +2,14 @@
 
 Chart.defaults.font.family = 'Conv_Montserrat-Regular,Conv_Montserrat-Bold,Tahoma, Arial, sans-serif';
 var ctx = document.querySelector('.graph-reviews__canvas').getContext('2d');
-var gradient1 = ctx.createLinearGradient(20, 0, 20, 170);
-var gradient2 = ctx.createLinearGradient(200, 200, 280, 0);
-var gradient3 = ctx.createLinearGradient(0, 90, 0, 160);
-var gradient4 = ctx.createLinearGradient(100, 0, 100, 150);
-var gradient1Legend = ctx.createLinearGradient(10, 0, 10, 10);
-var gradient2Legend = ctx.createLinearGradient(10, 0, 10, 10);
-var gradient3Legend = ctx.createLinearGradient(10, 0, 10, 10);
-var gradient4Legend = ctx.createLinearGradient(10, 0, 5, 5);
+var gradient1 = ctx.createLinearGradient(54, 46, 54, 163);
+var gradient2 = ctx.createLinearGradient(120, 45, 120, 100);
+var gradient3 = ctx.createLinearGradient(110, 106, 110, 161);
+var gradient4 = ctx.createLinearGradient(100, 100, 100, 250);
+var gradient1Legend = ctx.createLinearGradient(182, 79, 182, 87);
+var gradient2Legend = ctx.createLinearGradient(182, 100, 182, 111);
+var gradient3Legend = ctx.createLinearGradient(182, 123, 182, 135);
+var gradient4Legend = ctx.createLinearGradient(182, 147, 182, 159);
 gradient1.addColorStop(0, '#FFE39C');
 gradient1.addColorStop(1, '#FFBA9C');
 gradient2.addColorStop(0, '#6FCF97');
@@ -24,8 +24,8 @@ gradient2Legend.addColorStop(0, '#6FCF97');
 gradient2Legend.addColorStop(1, '#66D2EA');
 gradient3Legend.addColorStop(0, '#BC9CFF');
 gradient3Legend.addColorStop(1, '#8BA4F9');
-gradient4Legend.addColorStop(0, '#000000');
-gradient4Legend.addColorStop(1, '#000000');
+gradient4Legend.addColorStop(0, '#909090');
+gradient4Legend.addColorStop(1, '#3D4975');
 var myChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
@@ -33,7 +33,7 @@ var myChart = new Chart(ctx, {
     datasets: [{
       label: 'My First Dataset',
       data: [130, 65, 65, 0],
-      backgroundColor: [gradient1, gradient3, gradient2, gradient4]
+      backgroundColor: [gradient1, gradient2, gradient3, gradient4]
     }]
   },
   options: {
@@ -70,5 +70,16 @@ var myChart = new Chart(ctx, {
         }
       }
     }
-  }
+  },
+  plugins: [{
+    beforeDraw: function beforeDraw(c) {
+      var legends = c.legend.legendItems;
+      var gradients = [gradient1Legend, gradient2Legend, gradient3Legend, gradient4Legend];
+      var i = 0;
+      legends.forEach(function (e) {
+        e.fillStyle = gradients[i];
+        i++;
+      });
+    }
+  }]
 });
--- src/blocks/graph-reviews/graph-reviews.js
+++ src/blocks/graph-reviews/graph-reviews.js
@@ -1,13 +1,13 @@
 Chart.defaults.font.family = 'Conv_Montserrat-Regular,Conv_Montserrat-Bold,Tahoma, Arial, sans-serif';
 const ctx = document.querySelector('.graph-reviews__canvas').getContext('2d');
-var gradient1 = ctx.createLinearGradient(20,0,20,170);
-var gradient2 = ctx.createLinearGradient(200,200,280,0);
-var gradient3 = ctx.createLinearGradient(0,90,0,160);
-var gradient4 = ctx.createLinearGradient(100,0, 100,150);
-var gradient1Legend = ctx.createLinearGradient(10,0, 10,10);
-var gradient2Legend = ctx.createLinearGradient(10,0, 10,10);
-var gradient3Legend = ctx.createLinearGradient(10,0, 10,10);
-var gradient4Legend = ctx.createLinearGradient(10,0, 5,5);
+var gradient1 = ctx.createLinearGradient(54,46,54,163);
+var gradient2 = ctx.createLinearGradient(120,45,120,100);
+var gradient3 = ctx.createLinearGradient(110,106,110,161);
+var gradient4 = ctx.createLinearGradient(100,100, 100,250);
+var gradient1Legend = ctx.createLinearGradient(182,79, 182,87);
+var gradient2Legend = ctx.createLinearGradient(182,100, 182,111);
+var gradient3Legend = ctx.createLinearGradient(182,123, 182,135);
+var gradient4Legend = ctx.createLinearGradient(182,147, 182,159);
 gradient1.addColorStop(0,'#FFE39C');
 gradient1.addColorStop(1,'#FFBA9C');
 gradient2.addColorStop(0,'#6FCF97');
@@ -22,8 +22,8 @@ gradient2Legend.addColorStop(0,'#6FCF97');
 gradient2Legend.addColorStop(1,'#66D2EA');
 gradient3Legend.addColorStop(0,'#BC9CFF');
 gradient3Legend.addColorStop(1,'#8BA4F9');
-gradient4Legend.addColorStop(0,'#000000');
-gradient4Legend.addColorStop(1,'#000000');
+gradient4Legend.addColorStop(0,'#909090');
+gradient4Legend.addColorStop(1,'#3D4975');
 const myChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
@@ -38,8 +38,8 @@ const myChart = new Chart(ctx, {
       data: [130, 65, 65, 0],
       backgroundColor: [
         gradient1,
-        gradient3,
         gradient2,
+        gradient3,
         gradient4
       ]
     }]
@@ -79,5 +79,16 @@ const myChart = new Chart(ctx, {
       }
 
       }
-    }
+    },
+    plugins:[{
+      beforeDraw: function(c) {
+          var legends = c.legend.legendItems;
+          let gradients=[gradient1Legend,gradient2Legend,gradient3Legend,gradient4Legend]
+          let i=0;
+          legends.forEach(function(e) {
+            e.fillStyle = gradients[i];
+            i++;
+          });
+        }
+    }]
 })
