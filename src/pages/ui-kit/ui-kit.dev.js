7bb493488c4aeac6df8a73682293d7704a4c093c
Andrey_Gudin <aeon150791@gmail.com>
2021-11-29 16:40:35 +0400

    Fixing guesst-dropdown style

---
+++ src/pages/ui-kit/ui-kit.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("../../blocks/guest-dropdown/index.js");
+
+require("../../blocks/guest-dropdown/guest-dropdown.js");
+
+require("../../blocks/guest-dropdown/--rooms/guest-dropdown--rooms.js");
+
+require("../../blocks/textfield/textfield--date-dropdown/datepicker.min.js");
+
+require("../../blocks/textfield/textfield--date-dropdown/textfield--date-dropdown.js");
+
+require("../../blocks/textfield/textfield--filter-date-dropdown/textfield--filter-date-dropdown.js");
+
+require("../../blocks/like-button/like-button.js");
+
+require("../../blocks/range-slider/nouislider.js");
+
+require("../../blocks/range-slider/range-slider.js");
+
+require("../../blocks/pagination/pagination.min.js");
+
+require("../../blocks/pagination/paginationblock.js");
+
+require("../../blocks/graph-reviews/chart.min.js");
+
+require("../../blocks/graph-reviews/graph-reviews.js");
