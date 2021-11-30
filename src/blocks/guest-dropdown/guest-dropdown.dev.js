b23b26bdae7ba482c54e2e5899dad84e8dfb179d
Andrey_Gudin <aeon150791@gmail.com>
2021-11-30 14:49:34 +0400

    Cleaning scss of guest-dropdown

--- src/blocks/guest-dropdown/guest-dropdown.dev.js
+++ src/blocks/guest-dropdown/guest-dropdown.dev.js
@@ -3,11 +3,11 @@
 $(document).ready(function () {
   $(".guest-dropdown").iqDropdown({
     setSelectionText: function setSelectionText(itemCount, totalItems) {
-      var t = ['гость', 'гостя', 'гостей'];
-      if (totalItems == 0) return 'Сколько гостей';
-      if (totalItems == 1) return totalItems + ' ' + t[0];
-      if (totalItems > 1 && totalItems < 5) return totalItems + ' ' + t[1];
-      if (totalItems > 4) return totalItems + ' ' + t[2];
+      var t = ["гость", "гостя", "гостей"];
+      if (totalItems == 0) return "Сколько гостей";
+      if (totalItems == 1) return totalItems + " " + t[0];
+      if (totalItems > 1 && totalItems < 5) return totalItems + " " + t[1];
+      if (totalItems > 4) return totalItems + " " + t[2];
     }
   });
 });
--- src/blocks/guest-dropdown/guest-dropdown.js
+++ src/blocks/guest-dropdown/guest-dropdown.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".guest-dropdown").iqDropdown({
     setSelectionText: function (itemCount, totalItems) {
-      let t=['гость','гостя','гостей'];
-      if (totalItems == 0) return 'Сколько гостей';
-      if (totalItems == 1) return totalItems + ' ' + t[0];
-      if (totalItems >1 && totalItems < 5) return totalItems + ' ' + t[1];
-      if (totalItems > 4) return totalItems + ' ' + t[2];
-    }
+      let t = ["гость", "гостя", "гостей"];
+      if (totalItems == 0) return "Сколько гостей";
+      if (totalItems == 1) return totalItems + " " + t[0];
+      if (totalItems > 1 && totalItems < 5) return totalItems + " " + t[1];
+      if (totalItems > 4) return totalItems + " " + t[2];
+    },
   });
 });
