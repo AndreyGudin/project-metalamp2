0350ff638384fb8894689c0af6462bbc2d2769c8
Andrey_Gudin <aeon150791@gmail.com>
2021-12-03 08:54:52 +0400

    Prettify scss

--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,16 +1,20 @@
- $(document).on("click",".like-button__button.add",function() {
-    $(".like-button__button").addClass("active").removeClass("add");
-    $(".like-button__span").html(function(numberp){
-      numberp=parseInt($(".like-button__span").html());
-      numberp++;
-      return numberp.toString();
-    });
-  })
+$(document).on("click", ".like-button__button.add", function () {
+  $(".like-button__button").addClass("active").removeClass("add");
+  $(".like-button__span").html(function (numberp) {
+    numberp = parseInt($(".like-button__span").html());
+    numberp++;
+    return numberp.toString();
+  });
+});
 
-  $(document).on("click",".active",function() {
-    $(".like-button__span").html(function(numberp){
-      numberp=parseInt($(".like-button__span").html());
-      if (numberp>0) {numberp--;console.log(numberp);return numberp.toString();}
-    });
-    $(".like-button__button").removeClass("active").addClass("add");
-  })
+$(document).on("click", ".active", function () {
+  $(".like-button__span").html(function (numberp) {
+    numberp = parseInt($(".like-button__span").html());
+    if (numberp > 0) {
+      numberp--;
+      console.log(numberp);
+      return numberp.toString();
+    }
+  });
+  $(".like-button__button").removeClass("active").addClass("add");
+});
