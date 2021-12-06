1245c6755b2e3ba147e4a280e9163db8c69ffe5b
Andrey_Gudin <aeon150791@gmail.com>
2021-12-06 13:11:18 +0400

    Minor changes

--- src/blocks/range-slider/range-slider.dev.js
+++ src/blocks/range-slider/range-slider.dev.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var slider = document.querySelector('.range-slider__slider');
-var rangeSliderValueElement = document.querySelector('.range-slider__range');
+var slider = document.querySelector(".range-slider__slider");
+var rangeSliderValueElement = document.querySelector(".range-slider__range");
 noUiSlider.create(slider, {
   start: [2000, 8000],
   connect: true,
   step: 500,
   range: {
-    'min': 0,
-    'max': 20000
+    min: 0,
+    max: 20000
   }
 });
-slider.noUiSlider.on('update', function (values) {
-  rangeSliderValueElement.innerHTML = values.join('₽ - ').concat('₽');
+slider.noUiSlider.on("update", function (values) {
+  rangeSliderValueElement.innerHTML = values.join("₽ - ").concat("₽");
 });
