43487552d119bbcbd39dac59d6b6c55d488dd8bd
Andrey_Gudin <aeon150791@gmail.com>
2021-12-13 16:27:34 +0400

    Finished room-card

--- src/blocks/room-card/room-card.dev.js
+++ src/blocks/room-card/room-card.dev.js
@@ -3,7 +3,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   var splide = new Splide('.splide', {
     width: 271,
-    height: 152
+    height: 152,
+    type: 'loop',
+    cover: 'true'
   });
   splide.mount();
 });
--- src/blocks/room-card/room-card.js
+++ src/blocks/room-card/room-card.js
@@ -2,6 +2,8 @@ document.addEventListener( 'DOMContentLoaded', function() {
   var splide = new Splide( '.splide',{
     width: 271,
     height: 152,
+    type: 'loop',
+    cover: 'true',
   } );
   splide.mount();
 } );
