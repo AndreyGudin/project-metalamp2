992cecb68a5c0374eaefec76db7f829019492997
Andrey_Gudin <aeon150791@gmail.com>
2021-12-29 13:00:32 +0400

    Added hide to button of calendar, border for footer

--- src/blocks/guest-dropdown/guest-dropdown.js
+++ src/blocks/guest-dropdown/guest-dropdown.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
   $(".guest-dropdown,.booking-room-guest-dropdown").iqDropdown({
     setSelectionText: function (itemCount, totalItems) {
       let t = ["гость", "гостя", "гостей"];
+      let returnString='';
+      let guestsCount=itemCount['item1'] + itemCount['item2'];
       if (totalItems == 0) return "Сколько гостей";
-      if (totalItems == 1) return totalItems + " " + t[0];
-      if (totalItems > 1 && totalItems < 5) return totalItems + " " + t[1];
-      if (totalItems > 4) return totalItems + " " + t[2];
+      if (totalItems == 1) return "1 гость";
+      if ((guestsCount > 1)&&  (guestsCount < 5)) 
+        returnString += `${itemCount['item1'] + itemCount['item2']} гостя`; 
+        if (guestsCount > 4) returnString += `${itemCount['item1'] + itemCount['item2']} гостей`;
+      if (itemCount['item3']==1) returnString +=`, ${itemCount['item3']} младенец`;
+      if (itemCount['item3']>1) returnString +=`, ${itemCount['item3']} младенца`;
+      return returnString;
     },
   });
 });
