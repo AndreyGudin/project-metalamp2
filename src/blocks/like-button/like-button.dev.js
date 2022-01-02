b9843348869f86a852ea9d2303f511e3abb459b0
AndreyGudin <aeon150791@gmail.com>
2022-01-02 14:31:44 +0400

    Fixed like-button

--- src/blocks/like-button/like-button.dev.js
+++ src/blocks/like-button/like-button.dev.js
@@ -1,21 +1,22 @@
 "use strict";
 
 $(document).on("click", ".like-button__button.add", function () {
-  $(".like-button__button").addClass("active").removeClass("add");
-  $(".like-button__span").html(function (numberp) {
-    numberp = parseInt($(".like-button__span").html());
+  $(this).addClass("active").removeClass("add");
+  $(this).children("span").html(function (numberp) {
+    numberp = parseInt($(this).text());
+    console.log($(this).text());
     numberp++;
     return numberp.toString();
   });
 });
 $(document).on("click", ".active", function () {
-  $(".like-button__span").html(function (numberp) {
-    numberp = parseInt($(".like-button__span").html());
+  $(this).children("span").html(function (numberp) {
+    numberp = parseInt($(this).text());
 
     if (numberp > 0) {
       numberp--;
       return numberp.toString();
     }
   });
-  $(".like-button__button").removeClass("active").addClass("add");
+  $(this).removeClass("active").addClass("add");
 });
