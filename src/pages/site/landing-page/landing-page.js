7ea317115b7724b81d72f148c535df414f0f59c5
AndreyGudin <aeon150791@gmail.com>
2022-02-25 17:05:23 +0400

    Added landing-page bundle

---
+++ src/pages/site/landing-page/landing-page.js
@@ -0,0 +1,16 @@
+import AirDatepicker from 'air-datepicker';
+
+import * as itemQuantityDropdown from '../../../blocks/guest-dropdown/item-quantity-dropdown.js';
+
+import {headerBurgerMenu} from "../../../blocks/header/header.js";
+import {textfieldDateDropdown} from './../../../blocks/textfield/textfield_date-dropdown/textfield_date-dropdown.js';
+import {guestDropdown} from './../../../blocks/guest-dropdown/guest-dropdown.js';
+
+headerBurgerMenu('.header');
+guestDropdown();
+window.AirDatepicker = AirDatepicker;
+window.onload = function(){
+  textfieldDateDropdown('.textfield__input.textfield_date-dropdown__input.date-arrival__input');
+  textfieldDateDropdown('.textfield__input.textfield_date-dropdown__input.date-exit__input');
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,15 @@ module.exports={
     clean: true,
   },
   entry:{
-    uiforms:'./src/pages/ui-kit/ui-forms/ui-forms.pug',
+    uiForms:'./src/pages/ui-kit/ui-forms/ui-forms.pug',
     'assets/js/uiforms':'./src/pages/ui-kit/ui-forms/ui-kit.js',
-    uicards:'./src/pages/ui-kit/ui-cards/ui-cards.pug',
+    uiCards:'./src/pages/ui-kit/ui-cards/ui-cards.pug',
     'assets/js/uicards':'./src/pages/ui-kit/ui-cards/ui-cards.js',
-    footerheader:'./src/pages/ui-kit/footer-header/footer-header.pug',
-    'assets/js/footerheader':'./src/pages/ui-kit/footer-header/footer-header.js'
+    footerHeader:'./src/pages/ui-kit/footer-header/footer-header.pug',
+    'assets/js/footerheader':'./src/pages/ui-kit/footer-header/footer-header.js',
+    landingPage:'./src/pages/site/landing-page/landing-page.pug',
+    'assets/js/landing-page':'./src/pages/site/landing-page/landing-page.js',
+
   },
   devtool: 'source-map',
   plugins:[
